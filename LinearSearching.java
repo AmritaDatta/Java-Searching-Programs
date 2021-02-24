810ed89a3c5fbd85d7d195804e40e27a922997ae
Amrita Datta <78852592+AmritaDatta@users.noreply.github.com>
2021-02-24 17:45:24 +0100

    Update LinearSearching.java
    Linear Search in Java for Integer Array

--- LinearSearching.java
+++ LinearSearching.java
@@ -1,15 +1,20 @@
-package linearsearching;
+package linearsearcheforinteger;
 
-public class LinearSearching {
+public class LinearSearcheForInteger {
 
     public static void main(String[] args) {
         int[] arr={2,4,6,8,9,14,32,51,3,6};
         int item=6;
+        int temp=o;
         
         for (int index=0; index<arr.length; index++){
             if (arr[index]==item){
-                System.out.println(index);
+                System.out.println("Element is at " + index + " position");
+                temp=temp+1;
             }
+        }
+        if (temp==0){
+           System.out.println("Element not found"); 
         }
     } 
 }
